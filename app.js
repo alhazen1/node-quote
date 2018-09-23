3da6c30eed795a68f3f7f3aeb66508f334618d12
d wilder <thereal.david.wilder@gmail.com>
2018-09-22 22:36:46 -0500

    Remove response headers for testing

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(limiter);
 
 
 app.enable('json escape');
-//app.set('jsonp callback monkey')
+//app.set('jsonp callback name', 'monkey')
 
 app.use(bodyParser.urlencoded({
     extended: false
@@ -37,8 +37,8 @@ app.use(bodyParser.json());
 
 //CORS stuff
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    //res.header('Access-Control-Allow-Origin', '*');
+    //res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
 
     if (req.method === 'OPTIONS') {
         //res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
